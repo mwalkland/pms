90a069c8ee035b88c489fb2aa71daf9f9e86f023
mwalkland <matthewwalkland@gmail.com>
2018-01-14 19:59:21 +0000

    NOTAG Clean up routing using middleware

--- server/routes/project.js
+++ server/routes/project.js
@@ -5,64 +5,56 @@ const jwt = require('jsonwebtoken');
 const Project = require('../../models/project');
 const User = require('../../models/user');
 
-// router.use('/', (req, res, next) => {
-//   jwt.verify(req.query.token, 'WO3V%oIBK5c2', (err) => {
-//     if (err) {
-//       return res.status(401).json({
-//         title: 'Not Authenticated',
-//         error: err
-//       });
-//     }
-//     next();
-//   });
-// });
-
-router.post('/new', (req, res) => {
-  const body = req.body;
-  jwt.verify(req.query.token, 'WO3V%oIBK5c2', (err, decoded) => {
+router.use('/', (req, res, next) => {
+  jwt.verify(req.query.token, 'WO3V%oIBK5c2', (err) => {
     if (err) {
       return res.status(401).json({
-        title: 'Not Authentication',
+        title: 'Not Authenticated',
         error: err
       });
     }
-    User.findById(decoded.user._id, (err, user) => {
+    next();
+  });
+});
 
+router.post('/new', (req, res) => {
+  const body = req.body;
+  const decoded = jwt.decode(req.query.token);
+  User.findById(decoded.user._id, (err, user) => {
+
+    if (err) {
+      return res.status(500).json({
+        title: 'An error occured getting the user',
+        error: err
+      });
+    }
+    const project = new Project({
+      name: body.name,
+      description: body.description,
+      type: body.type,
+      maxStudents: body.maxStudents,
+      areas: body.areas,
+      staff: user,
+      students: [],
+      status: 'available'
+    });
+
+    project.save((err, result) => {
       if (err) {
         return res.status(500).json({
-          title: 'An error occured getting the user',
+          title: 'An error occured creating Project',
           error: err
         });
       }
 
-      const project = new Project({
-        name: body.name,
-        description: body.description,
-        type: body.type,
-        maxStudents: body.maxStudents,
-        areas: body.areas,
-        staff: user,
-        students: [],
-        status: 'available'
-      });
+      User.update(
+        { _id: user._id },
+        { $push: { 'staffInfo.suggestedProjects': result } }
+      ).exec();
 
-      project.save((err, result) => {
-        if (err) {
-          return res.status(500).json({
-            title: 'An error occured creating Project',
-            error: err
-          });
-        }
-
-        User.update(
-          { _id: user._id },
-          { $push: { 'staffInfo.suggestedProjects': result } }
-        ).exec();
-
-        res.status(201).json({
-          message: 'Project created',
-          obj: result
-        });
+      res.status(201).json({
+        message: 'Project created',
+        obj: result
       });
     });
   });
@@ -84,21 +76,33 @@ router.get('/getAreas', (req, res) => {
 });
 
 router.get('/getAllProjects', (req, res) => {
-  jwt.verify(req.query.token, 'WO3V%oIBK5c2', (err) => {
+  Project.find().populate('staff').exec(
+    (err, projects) => {
+      if (err) {
+        return res.status(401).json({
+          title: 'Error retrieving projects',
+          error: err
+        });
+      }
+      res.status(200).json({
+        message: 'Success',
+        projects: projects
+      });
+    });
+});
+
+router.get('/getProjectRequests', (req, res) => {
+  const decoded = jwt.decode(req.query.token);
+  User.findById(decoded.user._id, (err, user) => {
     if (err) {
-      return res.status(401).json({
-        title: 'Not Authentication',
+      return res.status(500).json({
+        title: 'An error occured getting the user',
         error: err
       });
     }
-    Project.find().populate('staff').exec(
-      (err, projects) => {
-        if (err) {
-          return res.status(401).json({
-            title: 'Error retrieving projects',
-            error: err
-          });
-        }
+    Project.find({ staff: user, pendingStudents: { $exists: true, $ne: [] } })
+      .populate({ path: 'pendingStudents', select: 'firstname surname email' })
+      .exec((err, projects) => {
         res.status(200).json({
           message: 'Success',
           projects: projects
@@ -107,64 +111,33 @@ router.get('/getAllProjects', (req, res) => {
   });
 });
 
-router.get('/getProjectRequests', (req, res) => {
-  jwt.verify(req.query.token, 'WO3V%oIBK5c2', (err, decoded) => {
-    if (err) {
-      return res.status(401).json({
-        title: 'Not Authentication',
-        error: err
-      });
-    }
-    User.findById(decoded.user._id, (err, user) => {
-      if (err) {
-        return res.status(500).json({
-          title: 'An error occured getting the user',
-          error: err
-        });
-      }
-      Project.find({ staff: user, pendingStudents: { $exists: true, $ne: [] } })
-        .populate({ path: 'pendingStudents', select: 'firstname surname email' })
-        .exec((err, projects) => {
-          res.status(200).json({
-            message: 'Success',
-            projects: projects
-          });
-        });
-    });
-  });
+router.get('/getConfirmedProjects', (req, res) => {
+
 });
 
 router.patch('/confirmProject', (req, res) => {
   const body = req.body;
   const project = body.project;
   const student = body.student;
-  jwt.verify(req.query.token, 'WO3V%oIBK5c2', (err) => {
+
+  // note the different in id and _id
+  Project.findByIdAndUpdate(project.id, {
+    $pull: { pendingStudents: student._id },
+    $push: { students: student._id }
+  }, { new: true }, (err, project) => {
     if (err) {
-      return res.status(401).json({
-        title: 'Not Authentication',
+      return res.status(500).json({
+        title: 'An error occured updating the project information',
         error: err
       });
     }
-
-    // note the different in id and _id
-    Project.findByIdAndUpdate(project.id, {
-      $pull: { pendingStudents: student._id },
-      $push: { students: student._id }
-    }, { new: true }, (err, project) => {
-      if (err) {
-        return res.status(500).json({
-          title: 'An error occured updating the project information',
-          error: err
-        });
-      }
-      if (project.maxStudents === project.students.length) {
-        project.full = true;
-        project.save();
-      }
-    });
-    res.status(200).json({
-      message: 'Successfully updated the projects',
-    });
+    if (project.maxStudents === project.students.length) {
+      project.full = true;
+      project.save();
+    }
+  });
+  res.status(200).json({
+    message: 'Successfully updated the projects',
   });
 });
 
@@ -172,41 +145,32 @@ router.patch('/rejectProject', (req, res) => {
   const body = req.body;
   const project = body.project;
   const student = body.student;
-  jwt.verify(req.query.token, 'WO3V%oIBK5c2', (err) => {
+
+  User.findById(student._id, (err, student) => {
     if (err) {
-      return res.status(401).json({
-        title: 'Not Authentication',
+      return res.status(500).json({
+        title: 'Could not find student',
         error: err
       });
     }
-
-    User.findById(student._id, (err, student) => {
+    Project.findById(project.id, (err, project) => {
       if (err) {
         return res.status(500).json({
-          title: 'Could not find student',
+          title: 'Could not find project',
           error: err
         });
       }
-      Project.findById(project.id, (err, project) => {
-        if (err) {
-          return res.status(500).json({
-            title: 'Could not find project',
-            error: err
-          });
-        }
-        student.studentInfo.chosenProject = undefined;
-        project.pendingStudents.pull(student._id);
+      student.studentInfo.chosenProject = undefined;
+      project.pendingStudents.pull(student._id);
 
-        student.save();
-        project.save();
-      });
-    });
-
-    res.status(200).json({
-      message: 'Successfully rejected the project'
+      student.save();
+      project.save();
     });
   });
 
+  res.status(200).json({
+    message: 'Successfully rejected the project'
+  });
 });
 
 module.exports = router;
--- server/routes/user.js
+++ server/routes/user.js
@@ -34,39 +34,31 @@ router.get('/getStaff', (req, res) => {
 
 router.patch('/addStudentProject', (req, res) => {
   const body = req.body;
-  jwt.verify(req.query.token, 'WO3V%oIBK5c2', (err, decoded) => {
+  const decoded = jwt.decode(req.query.token);
+  const updateObj = {
+    $set: { status: 'pending' },
+    $push: { pendingStudents: decoded.user._id }
+  };
+
+  Project.findByIdAndUpdate(body.id, updateObj, { new: true }, (err, project) => {
     if (err) {
-      return res.status(401).json({
-        title: 'Not Authentication',
+      return res.status(500).json({
+        title: 'An error occured finding the project',
         error: err
       });
     }
 
-    const updateObj = {
-      $set: { status: 'pending' },
-      $push: { pendingStudents: decoded.user._id }
-    };
-
-    Project.findByIdAndUpdate(body.id, updateObj, { new: true }, (err, project) => {
+    User.findByIdAndUpdate(decoded.user._id, { $set: { 'studentInfo.chosenProject': project } }, (err) => {
       if (err) {
         return res.status(500).json({
-          title: 'An error occured finding the project',
+          title: 'An error occured updating the user',
           error: err
         });
       }
+    });
 
-      User.findByIdAndUpdate(decoded.user._id, { $set: { 'studentInfo.chosenProject': project } }, (err) => {
-        if (err) {
-          return res.status(500).json({
-            title: 'An error occured updating the user',
-            error: err
-          });
-        }
-      });
-
-      res.status(200).json({
-        message: 'Student has been updated',
-      });
+    res.status(200).json({
+      message: 'Student has been updated',
     });
   });
 });

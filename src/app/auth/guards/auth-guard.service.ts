8b9ff8abc216180e2ad14c996c2e13e2a5ac01b1
mwalkland <matthewwalkland@gmail.com>
2018-03-13 13:59:52 +0000

    Minor changes

--- src/app/auth/guards/auth-guard.service.ts
+++ src/app/auth/guards/auth-guard.service.ts
@@ -19,7 +19,6 @@ export class AuthGuard implements CanActivate {
     if (this.authService.isLoggedIn()) {
       return true;
     } else {
-      console.log(this.router.url)
       if (this.router.url !== '/') {
         this.dialog.open(ExpiredComponent);
         return false;
